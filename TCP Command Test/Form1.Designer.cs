9d0effe415c3810f59ced9b685de4a462aad45f3
eunggi <egkim@agos.co.kr>
2023-08-09 13:00:58 +0900

    upload project

---
+++ TCP Command Test/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TCP_Command_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tb_ip = new System.Windows.Forms.TextBox();
+            this.tb_port = new System.Windows.Forms.TextBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Command_list = new System.Windows.Forms.ListBox();
+            this.CommandList_Clear = new System.Windows.Forms.Button();
+            this.ReceiveData_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(399, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tb_ip
+            // 
+            this.tb_ip.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tb_ip.Location = new System.Drawing.Point(584, 36);
+            this.tb_ip.Name = "tb_ip";
+            this.tb_ip.Size = new System.Drawing.Size(158, 29);
+            this.tb_ip.TabIndex = 1;
+            // 
+            // tb_port
+            // 
+            this.tb_port.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tb_port.Location = new System.Drawing.Point(584, 71);
+            this.tb_port.Name = "tb_port";
+            this.tb_port.Size = new System.Drawing.Size(158, 29);
+            this.tb_port.TabIndex = 1;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(748, 71);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(79, 29);
+            this.btn_connect.TabIndex = 2;
+            this.btn_connect.Text = "연결";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(500, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(500, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PORT";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "전송";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Location = new System.Drawing.Point(12, 77);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(479, 328);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TabStop = false;
+            // 
+            // Command_list
+            // 
+            this.Command_list.FormattingEnabled = true;
+            this.Command_list.ItemHeight = 12;
+            this.Command_list.Location = new System.Drawing.Point(511, 113);
+            this.Command_list.Name = "Command_list";
+            this.Command_list.Size = new System.Drawing.Size(316, 292);
+            this.Command_list.TabIndex = 6;
+            this.Command_list.SelectedIndexChanged += new System.EventHandler(this.Command_list_SelectedIndexChanged);
+            // 
+            // CommandList_Clear
+            // 
+            this.CommandList_Clear.Location = new System.Drawing.Point(735, 414);
+            this.CommandList_Clear.Name = "CommandList_Clear";
+            this.CommandList_Clear.Size = new System.Drawing.Size(91, 24);
+            this.CommandList_Clear.TabIndex = 7;
+            this.CommandList_Clear.Text = "Clear";
+            this.CommandList_Clear.UseVisualStyleBackColor = true;
+            this.CommandList_Clear.Click += new System.EventHandler(this.CommandList_Clear_Click);
+            // 
+            // ReceiveData_Clear
+            // 
+            this.ReceiveData_Clear.Location = new System.Drawing.Point(400, 411);
+            this.ReceiveData_Clear.Name = "ReceiveData_Clear";
+            this.ReceiveData_Clear.Size = new System.Drawing.Size(91, 24);
+            this.ReceiveData_Clear.TabIndex = 7;
+            this.ReceiveData_Clear.Text = "Clear";
+            this.ReceiveData_Clear.UseVisualStyleBackColor = true;
+            this.ReceiveData_Clear.Click += new System.EventHandler(this.ReceiveData_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 453);
+            this.Controls.Add(this.ReceiveData_Clear);
+            this.Controls.Add(this.CommandList_Clear);
+            this.Controls.Add(this.Command_list);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.tb_port);
+            this.Controls.Add(this.tb_ip);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "TCP TEST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button CommandList_Clear;
+        private System.Windows.Forms.Button ReceiveData_Clear;
+
+        private System.Windows.Forms.ListBox Command_list;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox tb_port;
+        private System.Windows.Forms.Button btn_connect;
+
+        private System.Windows.Forms.TextBox tb_ip;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ TCP Command Test/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_Command_Test
+{
+    public partial class Form1 : Form
+    {
+        private bool isConnected = false;
+        private TcpClient tcpClient;
+        private static StreamReader streamReader;
+        private static StreamWriter streamWriter;
+        string serverIp = "192.168.123.102";
+        int serverPort = 5025;
+        private byte[] readBuffer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tb_ip.Text = serverIp;
+            tb_port.Text = serverPort.ToString();
+
+            // 서버의 IP 주소와 포트 번호
+        }
+
+        void SendData(TcpClient client, string data)
+        {
+            // 데이터 전송을 위한 스트림 생성
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(data);
+                stream.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("데이터를 전송했습니다: " + data);
+            }
+        }
+
+        string ReceiveData(TcpClient client)
+        {
+            // 데이터 수신을 위한 스트림 생성
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string receivedData = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                return receivedData;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // TCP 클라이언트 생성
+            
+            if (!isConnected)
+            {
+                var r = ConnectTcp();
+                if (!r) return;
+                btn_connect.Text = "연결해제";
+                isConnected = true;
+            }
+            else
+            {
+                Disconnect();
+                btn_connect.Text = "연결";
+                isConnected = false;
+            }
+                
+
+        }
+
+        async Task SendDataAsync(TcpClient client, string data)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(data);
+            await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+            Console.WriteLine("데이터를 비동기적으로 전송했습니다: " + data);
+        }
+
+        bool ConnectTcp()
+        {
+            //연결이 되어있지 않은 경우
+            if (tcpClient == null)
+            {
+                tcpClient = new TcpClient(); // TcpClient 객체 생성
+                try
+                {
+                    // 서버에 연결 요청
+                    if (!tcpClient.ConnectAsync(tb_ip.Text, Int32.Parse(tb_port.Text)).Wait(1000))
+                    {
+                        //connect timeout 처리
+                        tcpClient = null;
+                        return false;
+                    }
+                    // Debug.WriteLine("서버 연결됨...");
+                    streamReader = new StreamReader(tcpClient.GetStream()); // 읽기 스트림 연결
+                    streamWriter = new StreamWriter(tcpClient.GetStream()); // 쓰기 스트림 연결
+                    streamWriter.AutoFlush = true; //자동으로 버퍼 비워줌.
+                    //전역변수로 연결되었는지를 설정
+
+                    SEND_COMMAND("*IDN?");
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            //이미 연결되어있을 경우.
+            if (tcpClient.Connected)
+            {
+                //전역변수로 연결되었는지를 설정
+                return true;
+            }
+            
+            tcpClient = null;
+            return false;
+        }
+
+        // void receiveData()
+        // {
+        //     while (true)
+        //     {
+        //         client.GetStream().Read(readBuffer, 0, 1024);
+        //         Console.WriteLine(readBuffer);
+        //     }
+        // }
+        
+        public string SEND_COMMAND(string com)
+        {
+            //Debug.WriteLine("send com : " + com);
+            string str = null;
+            if (tcpClient != null)
+            {
+                if (tcpClient.Connected) // 클라이언트가 연결되어 있는 동안
+                {
+                    streamWriter.WriteLine(com);
+                    while (str == null)
+                    {
+                        str = streamReader.ReadLine(); // 수신 데이타를 읽어서 receiveData1 변수에 저장
+                        continue;
+                    }
+                }
+            }
+
+            textBox4.AppendText(str);
+            
+            return str;
+        }
+        
+        public void Disconnect()
+        {
+            streamReader.Close();
+            streamReader.Dispose();
+            streamWriter.Close();
+            streamWriter.Dispose();
+            tcpClient.Close();
+            tcpClient.Dispose();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                MessageBox.Show("TCP 연결 해주세요","알림",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
+            SEND_COMMAND(textBox1.Text);
+            Command_list.Items.Add(textBox1.Text);
+        }
+
+        private void Command_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = Command_list.SelectedItem.ToString();
+        }
+
+        private void ReceiveData_Clear_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+        }
+
+        private void CommandList_Clear_Click(object sender, EventArgs e)
+        {
+            Command_list.Items.Clear();
+        }
+    }
+}
